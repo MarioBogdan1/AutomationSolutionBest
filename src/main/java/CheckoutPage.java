3375087b80af40055f8c1796f23365977148618f
mariobogdan <mariobogdaniancu@gmail.com>
2025-03-26 17:32:39 +0200

    Initial commit

---
+++ src/main/java/CheckoutPage.java
@@ -0,0 +1,66 @@
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static java.lang.Double.parseDouble;
+
+
+// Declaring the CheckoutPage class, which extends the BasePage class.
+// By extending BasePage, CheckoutPage inherits the WebDriver instance and the PageFactory initialization.
+public class CheckoutPage extends BasePage {
+
+    // Declaring a public WebDriverWait variable named 'wait'.
+    // WebDriverWait is used for implementing explicit waits during interactions with web elements.
+    public WebDriverWait wait;
+
+    // Constructor for the CheckoutPage class that takes a WebDriver object as an argument.
+    // This constructor calls the parent class (BasePage) constructor to initialize the WebDriver instance
+    // and sets up the PageFactory to initialize the web elements on this page.
+    public CheckoutPage(WebDriver driver) {
+        // Calling the parent class (BasePage) constructor using 'super' to initialize the WebDriver.
+        super(driver);
+
+        // Initializing the WebDriverWait object with a 10-second timeout.
+        // This will be used to wait for certain conditions or elements during test execution.
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    // Locating the search bar element using the @FindBy annotation.
+    // @FindBy is a Selenium annotation that helps locate elements on the web page.
+    // Here, the element is being located by its 'id' attribute with the value "input-search".
+    // Declare the WebElement as private to enforce encapsulation
+    // This ensures that 'searchBar' cannot be accessed directly from outside this class
+    @FindBy(id = "input-search")
+    private WebElement searchBar;
+
+    // A public method to set a value in the search bar.
+    // This method interacts with the searchBar element and sends the text "mouse" to it.
+    // Public method to interact with the private 'searchBar' element
+    // Provides controlled access to the encapsulated WebElement
+    public void setSearchBar() {
+        // Typing the word "mouse" into the search bar.
+        searchBar.sendKeys("mouse");
+    }
+
+    // Locating the search button element using the @FindBy annotation.
+    // The element is being located by its CSS selector, which identifies elements based on their classes.
+    // Here, the button has the classes "btn", "btn-light", and "btn-sm".
+    @FindBy(css = ".btn.btn-light.btn-sm")
+    private WebElement searchButton;
+
+    public void clickSearchButton() {
+        searchButton.click();
+    }
+
+    @FindBy(linkText = "Refined Frozen Mouse")
+    private WebElement frozenMouse;
+
+    public WebElement getFrozenMouse() {
+        return frozenMouse;
+    }
+
+}
---
+++ src/test/java/CheckoutTest.java
@@ -0,0 +1,47 @@
+import com.aventstack.extentreports.Status;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+// This annotation is used to specify that the ExtentTestNGITestListener class should be used to listen to the test events.
+// The listener will generate reports based on the test results using the ExtentReports library.
+@Listeners(ExtentTestNGITestListener.class)
+
+// Declaring the CheckoutTest class, which extends the Hooks class.
+// By extending Hooks, CheckoutTest inherits the setup and teardown methods for WebDriver.
+public class CheckoutTest extends Hooks {
+
+    // Declaring a public variable of type CheckoutPage named 'checkoutPage'.
+    // This will be used to interact with the CheckoutPage object during the tests.
+    public CheckoutPage checkoutPage;
+
+    // Declaring a public variable of type WebDriverWait named 'wait'.
+    // WebDriverWait is used to explicitly wait for certain conditions or elements during test execution.
+    public WebDriverWait wait;
+
+    // Method annotated with @BeforeMethod, indicating that it will run before each test method.
+    // This method is used to set up the page objects and other necessary components before each test.
+    @BeforeMethod
+    public void SetupPageObject() {
+
+        // Initializing the checkoutPage object with the current WebDriver instance.
+        // This allows the test methods to interact with elements on the checkout page.
+        checkoutPage = new CheckoutPage(driver);
+
+        // Initializing the WebDriverWait object with the current WebDriver instance and a timeout of 30 seconds.
+        // This wait will be used to pause the execution until certain conditions are met or elements are found.
+        wait = new WebDriverWait(driver, 30);
+    }
+
+
+    @Test(description = "Tests the search functionality by searching for the keyword 'mouse'")
+    public void searchTest() {
+        checkoutPage.setSearchBar();
+        checkoutPage.clickSearchButton();
+        ExtentTestNGITestListener.getTest().log(Status.INFO, "The search engine is looking up for the keyword 'mouse'");
+    }
+}
+
